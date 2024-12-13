98c420ccb3bcda4bf73f5f4fb21b15429c0e73df
Julian K. Arni <jkarni@gmail.com>
2024-12-13 12:33:33 +0100

    Have separate apps

--- flake.nix
+++ flake.nix
@@ -1,16 +1,25 @@
 {
   inputs = {
     garnix-lib.url = "github:garnix-io/garnix-lib";
+    dream2nix.url = "github:garnix-io/garnix-lib";
     nodejs-module.url = "github:garnix-io/nodejs-module";
+    nodejs-module.inputs.dream2nix.follows = "dream2nix";
   };
   outputs = inputs: inputs.garnix-lib.lib.mkModules {
     modules = [
       inputs.nodejs-module.garnixModules.default
     ];
     config = {
-      nodejs.backend = {
-        src = ./.;
+      nodejs.nextjs = {
+        src = ./next-js;
         serverCommand = "sample-nodejs-project";
+        testCommand = "npm run lint";
+        prettier = true;
+      };
+      nodejs.remix = {
+        src = ./remix;
+        serverCommand = "sample-nodejs-project";
+        testCommand = "npm run lint";
         prettier = true;
       };
     };
