4914350ceb421efda4cf2a36f5034f2b7f75b5d0
Julian K. Arni <jkarni@gmail.com>
2024-12-15 16:56:54 +0100

    Prettier fixes

--- remix/test.js
+++ remix/test.js
@@ -1,12 +1,12 @@
 const execSync = require("child_process").execSync;
 
-test('very basic sanity check', () => {
+test("very basic sanity check", () => {
   expect(1 + 2).toBe(3);
 });
 
-test('buildDependencies should be available', () => {
+test("buildDependencies should be available", () => {
   const out = execSync("hello", {
-    encoding: 'utf8'
+    encoding: "utf8",
   });
   expect(out).toBe("Hello, world!\n");
 });
